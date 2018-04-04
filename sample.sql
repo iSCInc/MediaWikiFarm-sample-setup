c5b0365ac77a325f393f5ba34b3e9df1e9f94bea
Suriyaa ✌️️ <isc.suriyaa@gmail.com>
2018-04-04 10:48:55 +0200

    Create sample.sql

---
+++ sample.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE tintinmoulinsart;
+CREATE USER tintinmoulinsart@localhost IDENTIFIED BY 'tintin';
+GRANT ALL ON tintinmoulinsart.* TO tintinmoulinsart@localhost;
+
+CREATE DATABASE miloumoulinsart;
+CREATE USER miloumoulinsart@localhost IDENTIFIED BY 'milou';
+GRANT ALL ON miloumoulinsart.* TO miloumoulinsart@localhost;
